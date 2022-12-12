16cb88c75666b7f815672efad190d32a79b9aed1
Evgeniy <golowin056@gmail.com>
2022-12-12 17:54:57 +0200

    create form sing in and error

--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -1,10 +1,13 @@
 import {useRouteError} from "react-router-dom";
 
+
+import './error.css';
+
 export const ErrorPage = () => {
     // const error = useRouteError();
 
     return (
-        <div id="error-page">
+        <div className="error-page" id="error-page">
             <h1>Oops!</h1>
             <p>Sorry, an unexpected error has occurred.</p>
             <p>
