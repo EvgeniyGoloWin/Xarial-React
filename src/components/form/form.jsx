bf0610f851c480ae5ff77a1d511362112fe2c5ce
Valentin130493 <gerasimenko130493@gmail.com>
2022-12-13 11:21:56 +0200

    css fix

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -7,15 +7,15 @@ const Form = () => {
     return (
         <form className={"form"}>
             <div className={"form_group"}>
-                <label>Email address</label>
+                <label className={"label"}>Email address</label>
                 <input className={"input"} type="text"/>
             </div>
             <div className={"form_group"}>
-                <label>Password</label>
+                <label className={"label"}>Password</label>
                 <input className={"input"} type="password"/>
             </div>
             <div className='form_btn'>
-            <button className='btn_forgot'> Forgot password</button>
+            <button className='btn_forgot'> Forgot password?</button>
             <button className='btn_singIn'> Sing in</button>
             </div>
         </form>
--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -1,18 +1,14 @@
-import {useRouteError} from "react-router-dom";
-
+import React from "react"
 
 import './error.css';
 
 export const ErrorPage = () => {
-    // const error = useRouteError();
+
 
     return (
         <div className="error-page" id="error-page">
             <h1>Oops!</h1>
             <p>Sorry, an unexpected error has occurred.</p>
-            <p>
-                {/*<i>{error.statusText || error.message}</i>*/}
-            </p>
         </div>
     );
 }
