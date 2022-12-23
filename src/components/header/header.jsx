87d7c6a1f9deb1a0c8c50207a5454604163c782c
Valentin130493 <gerasimenko130493@gmail.com>
2022-12-23 10:48:00 +0200

    add admin navigation btn

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {useLocation, useNavigate} from "react-router-dom";
-import {token} from "../../constants/storageKey";
+import {Link, useLocation, useNavigate} from "react-router-dom";
+import {token, role} from "../../constants/storageKey";
 
 import "./header.css"
 
@@ -9,7 +9,7 @@ const Header = () => {
     const navigate = useNavigate()
     const location = useLocation()
     const auth = sessionStorage.getItem(token)
-
+    const admin = sessionStorage.getItem(role)
     const logOut = () => {
         sessionStorage.clear()
         navigate('/')
@@ -26,6 +26,7 @@ const Header = () => {
                 <p className={"goHome"} onClick={goHome}>Xarial CMS</p>
             </div>
             <div>
+                {(admin && location.pathname !== "/admin") && <Link className={"link"} href={"/admin"}>admin</Link>}
                 {auth ? <button onClick={logOut} className={"btn"}>Log out</button> :
                     <button onClick={singIn} className={"btn"}>Sing in</button>}
             </div>
