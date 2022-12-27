58dd0a4e9086569484d8844d90e40823cb1c156c
Valentin130493 <gerasimenko130493@gmail.com>
2022-12-27 13:16:59 +0200

    change sending data from edit status page

--- src/pages/editStatus/editStatus.jsx
+++ src/pages/editStatus/editStatus.jsx
@@ -7,30 +7,32 @@ import {json} from "react-router";
 
 const EditStatus = () => {
     const inputRef = useRef(null)
-    const [state,setState] = useState([])
+    const [state, setState] = useState([])
 
     useEffect(() => {
         fetch(`${baseUrl}/status`)
             .then(res => res.json())
             .then((res) => {
                 setState(res)
-            })},[])
+            })
+    }, [])
 
     const handleClickAdd = async () => {
-        await setState(prev => [...state,inputRef.current.value])
+        await setState(prev => [...state, inputRef.current.value])
         inputRef.current.value = '';
     }
 
     const handleClickRemove = (name) => {
         const arr = state.filter((item) => item !== name)
-         setState(arr)
+        setState(arr)
     }
 
     const handleClickSave = async () => {
-        const test = JSON.stringify(state)
-        const res = await fetch(`${baseUrl}/status`, {
+
+        const res = await fetch(`http://localhost:8080/status`, {
             method: 'PUT',
-            body: test
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(state)
         });
         const data = await res.json()
         setState(data)
@@ -38,21 +40,21 @@ const EditStatus = () => {
 
     return (
         <>
-          <Header/>
+            <Header/>
 
             <div className="editPage">
                 <div className="add_status">
-                    <input className="input_status" ref={inputRef} />
+                    <input className="input_status" ref={inputRef}/>
                     <button className="btn" onClick={handleClickAdd}>Add</button>
                 </div>
                 {state?.map((item, index) => {
-                return <div key={`${index}`} className="status"> <p>
-                    {item}
-                </p>
-                    <button onClick={()=>handleClickRemove(item)} className="remove" type={"button"}>X
-                    </button>
-                </div>
-            })}
+                    return <div key={`${index}`} className="status"><p>
+                        {item}
+                    </p>
+                        <button onClick={() => handleClickRemove(item)} className="remove" type={"button"}>X
+                        </button>
+                    </div>
+                })}
                 <div className='section_btn'>
                     <button type="button" onClick={handleClickSave} className="btn">Save</button>
                 </div>
